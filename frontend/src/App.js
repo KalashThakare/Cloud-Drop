6f999c65310a60391ea30d1de2e0b7ac30e2c7a4
KalashT <kalashthakare898@gmail.com>
2025-03-10 19:07:42 +0530

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
 import "./FileUpload.css";
 import { axiosInstance } from "./lib/axios.js";
+import React, {useState} from "react";
 
-export default async function App() {
+export default function App() {
+
+  const [file,setFile] = useState();
 
   const handleSubmit=async(e)=>{
     e.preventDefault();
-    const data={
-      
+    const data = new FormData();
+    data.append("image",file)
+
+    const response = await axiosInstance.post("/func/upload",data,{headers:{"Content-Type":'multipart/form-data'}});
+
+    if(response.status===200){
+      console.log("Success")
     }
+    
   }
 
-  const response = await axiosInstance.post("/func/upload",data);
 
 
   return (
     <div className="file-upload-container">
-      <input type="file" id="fileInput" className="file-input" />
+      <input type="file" id="fileInput" className="file-input" onChange={(e)=>setFile(e.target.files[0])} accept="image/*"/>
       <label htmlFor="fileInput" className="file-label">
         Choose File
       </label>
-      <button  className="upload-file-button">
+      <button  className="upload-file-button" onSubmit={handleSubmit}>
         Upload File
       </button>
     </div>
