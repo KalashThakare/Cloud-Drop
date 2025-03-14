0434a15790e486c9c9c4b038d5f672f296a047dd
KalashT <kalashthakare898@gmail.com>
2025-03-14 16:14:05 +0530

    logout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,14 +3,17 @@ import { axiosInstance } from "@/lib/axios.js";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/store/useAuthStore";
+import { LogOut } from "lucide-react";
 
 export default function App() {
 
   const router = useRouter();
 
   const authUser = useAuthStore((state)=>state.authUser);
+  const logout = useAuthStore((state) => state.logout);
 
   useEffect(()=>{
+    console.log(authUser)
     if(authUser==null){
       router.replace("/login");
     }
@@ -32,33 +35,46 @@ export default function App() {
   };
 
   return (
-    <form 
-      onSubmit={submit} 
-      className="max-w-md mx-auto mt-12 p-6 rounded-xl bg-black shadow-lg flex flex-col gap-4 text-center border border-gray-700"
-    >
-      <h1 className="text-2xl font-bold mb-2 text-cyan-300">Cloud-drop</h1>
-
-      <input
-        onChange={(e) => setFile(e.target.files[0])}
-        type="file"
-        accept="image/*"
-        className="p-5 border-2 border-dashed border-gray-600 rounded-xl bg-gray-900 cursor-pointer text-white transition-all hover:border-blue-400 hover:bg-gray-800"
-      />
-
-      <input
-        value={caption}
-        onChange={(e) => setCaption(e.target.value)}
-        type="text"
-        placeholder="Caption"
-        className="p-3 border border-gray-600 rounded-lg text-lg text-white bg-gray-900 focus:border-blue-400 focus:outline-none transition-all"
-      />
-
+    <div className="relative min-h-screen">
       <button 
-        type="submit" 
-        className="p-3 bg-blue-600 text-white rounded-lg text-lg cursor-pointer transition-all hover:bg-blue-500 active:translate-y-0"
+        onClick={() => {
+          logout();
+          router.replace("/login");
+        }}
+        className="absolute top right-5 flex items-center gap-2 p-2 bg-red-600 text-white rounded-lg transition-all hover:bg-red-500"
       >
-        Upload
+        <LogOut size={20} />
+        Logout
       </button>
-    </form>
+      
+      <form 
+        onSubmit={submit} 
+        className="max-w-md mx-auto mt-12 p-6 rounded-xl bg-black shadow-lg flex flex-col gap-4 text-center border border-gray-700"
+      >
+        <h1 className="text-2xl font-bold mb-2 text-cyan-300">Cloud-drop</h1>
+  
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          accept="image/*"
+          className="p-5 border-2 border-dashed border-gray-600 rounded-xl bg-gray-900 cursor-pointer text-white transition-all hover:border-blue-400 hover:bg-gray-800"
+        />
+  
+        <input
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+          type="text"
+          placeholder="Caption"
+          className="p-3 border border-gray-600 rounded-lg text-lg text-white bg-gray-900 focus:border-blue-400 focus:outline-none transition-all"
+        />
+  
+        <button 
+          type="submit" 
+          className="p-3 bg-blue-600 text-white rounded-lg text-lg cursor-pointer transition-all hover:bg-blue-500 active:translate-y-0"
+        >
+          Upload
+        </button>
+      </form>
+    </div>
   );
 }
