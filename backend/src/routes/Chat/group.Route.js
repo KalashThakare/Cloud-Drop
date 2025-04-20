1388deefb8fbd3c7f7ccba849ff55bba3ce6941f
KalashT <kalashthakare898@gmail.com>
2025-04-20 23:03:47 +0530

    remove member route

--- backend/src/routes/Chat/group.Route.js
+++ backend/src/routes/Chat/group.Route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {protectRoute} from "../../middleware/auth.middleware.js"
-import { addMembersByEmail, createGroup } from "../../controller/Chat/group.js";
+import { addMembersByEmail, createGroup, removeMember } from "../../controller/Chat/group.js";
 
 const router  = express.Router();
 
 router.post("/create",protectRoute,createGroup);
 
 router.post("/:groupId/add-member",protectRoute,addMembersByEmail);
+
+router.post("/:groupId/remove-member",protectRoute,removeMember);
 
 export default router;
