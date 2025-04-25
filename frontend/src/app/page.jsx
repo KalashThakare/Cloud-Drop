1ff2bdac390f71bb309b7e0609ab883e1ac21f5e
KalashT <kalashthakare898@gmail.com>
2025-04-25 10:11:06 +0530

    bug fix

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import ScrollAnimation from "@/components/ScrollAnimation";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import Link from "next/link";
@@ -10,33 +9,25 @@ import { isTokenValid } from "../lib/utils.js";
 import { useAuthStore } from "@/store/useAuthStore.js";
 
 export default function Home() {
-
   const router = useRouter();
-  const checkAuth = useAuthStore(state => state.checkAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
 
-  useEffect(() => {
+  const handleClick = () => {
     const token = localStorage.getItem("authToken");
+    console.log(token);
     if (token && isTokenValid(token)) {
-      checkAuth(); // sets authUser if valid
-      router.push("/Main"); // redirect if token valid
+      checkAuth();
+      router.push("/Main?useDefault=true");
+    } else {
+      router.push("/Auth");
     }
-  }, []);
+  };
 
   const words = [
-    {
-      text: "AWS",
-    },
-    {
-      text: "S3",
-      className: "text-cyan-500 dark:text-cyan-500",
-    },
-    {
-      text: " with ",
-    },
-    {
-      text: "SecureShare",
-      className: "text-cyan-500 dark:text-cyan-500",
-    },
+    { text: "AWS" },
+    { text: "S3", className: "text-cyan-500 dark:text-cyan-500" },
+    { text: " with " },
+    { text: "SecureShare", className: "text-cyan-500 dark:text-cyan-500" },
   ];
 
   return (
@@ -51,115 +42,93 @@ export default function Home() {
           <a href="#blog" className="text-lg hover:underline">
             Blog
           </a>
-          {/* <button className="relative inline-flex h-10 w-28 overflow-hidden rounded-full p-[1px] shadow-2xl shadow-zinc-900 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 group">
-            <span className="absolute inset-0 overflow-hidden rounded-full">
-            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-            </span>
-            <div className="w-full relative flex items-center justify-center space-x-2 z-10 rounded-full bg-zinc-950 py-2 px-4 ring-1 ring-white/10">
-            <span className="text-sm font-semibold text-white">Login</span>
-            <svg
-                fill="none"
-                height="16"
-                viewBox="0 0 24 24"
-                width="16"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                d="M10.75 8.75L14.25 12L10.75 15.25"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                />
-            </svg>
-            </div>
-            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-500/0 via-cyan-500/90 to-cyan-500/0 transition-opacity duration-500 group-hover:opacity-40" />
-        </button> */}
-
         </nav>
       </header>
 
       <main className="flex flex-col items-center justify-center text-center px-6 py-20 mt-4">
         <h2 className="text-5xl md:text-6xl font-extrabold mb-6 text-white opacity-100">
           Unleash the power of <br />
-          <TypewriterEffectSmooth className={"text-center flex justify-center items-center"} words={words} />
+          <TypewriterEffectSmooth
+            className="text-center flex justify-center items-center"
+            words={words}
+          />
         </h2>
+
         <p className="text-lg md:text-xl text-gray-400 max-w-2xl mb-8">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout.
         </p>
-    <div className="space-x-4">
-        {/* Use Free Bucket Button */}
-        <Link href={{ pathname: "/Main", query: { useDefault: "true" } }}>
-        <button className="relative inline-flex h-12 w-48 overflow-hidden rounded-full p-[2px] shadow-2xl shadow-zinc-900 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 group">
-            
+
+        <div className="space-x-4">
+          {/* Use Free Bucket Button */}
+          <button
+            onClick={handleClick}
+            className="relative inline-flex h-12 w-48 overflow-hidden rounded-full p-[2px] shadow-2xl shadow-zinc-900 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 group"
+          >
             <span className="absolute inset-0 overflow-hidden rounded-full">
-            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
             </span>
             <div className="w-full relative flex items-center justify-center space-x-2 z-10 rounded-full bg-zinc-950 py-2 px-4 ring-1 ring-white/10">
-            <span className="text-sm font-semibold text-white">Use Free Bucket</span>
-            <svg
+              <span className="text-sm font-semibold text-white">
+                Use Free Bucket
+              </span>
+              <svg
                 fill="none"
                 height="16"
                 viewBox="0 0 24 24"
                 width="16"
                 xmlns="http://www.w3.org/2000/svg"
-            >
+              >
                 <path
-                d="M10.75 8.75L14.25 12L10.75 15.25"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
+                  d="M10.75 8.75L14.25 12L10.75 15.25"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
                 />
-            </svg>
+              </svg>
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-500/0 via-cyan-500/90 to-cyan-500/0 transition-opacity duration-500 group-hover:opacity-40" />
-            
-            
-        </button>
-        </Link>
+          </button>
 
-        {/* Use your own Bucket Button */}
-        <Link href={{ pathname: "/Main", query: { useDefault: "false" } }}>
-        <button className="relative inline-flex h-12 w-48 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00FFFF_0%,#007BFF_50%,#00FFFF_100%)]" />
-        <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            Use your own Bucket
-            </span>
-        </button>
-        </Link>
-    </div>
+          {/* Use your own Bucket Button */}
+          <Link href={{ pathname: "/Main", query: { useDefault: "false" } }}>
+            <button className="relative inline-flex h-12 w-48 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00FFFF_0%,#007BFF_50%,#00FFFF_100%)]" />
+              <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Use your own Bucket
+              </span>
+            </button>
+          </Link>
+        </div>
 
-    <div className="flex justify-center mt-12">
-        <a
-          href="#Dashboard"
-          className="text-gray-400 hover:text-white transition"
-        >
-          <span className="text-lg">Learn More</span>
-          <br />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mx-auto mt-2 animate-bounce"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+        <div className="flex justify-center mt-12">
+          <a
+            href="#Dashboard"
+            className="text-gray-400 hover:text-white transition"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </a>
-      </div>
+            <span className="text-lg">Learn More</span>
+            <br />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mx-auto mt-2 animate-bounce"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </a>
+        </div>
 
-
-      <ScrollAnimation />
-      <SubscriptionPlans />
-
-    </main>
+        <ScrollAnimation />
+        <SubscriptionPlans />
+      </main>
 
       <footer className="absolute bottom-0 w-full bg-gray-800 py-4 text-center text-gray-400">
         <p>© 2025 Cobalt. All rights reserved.</p>
