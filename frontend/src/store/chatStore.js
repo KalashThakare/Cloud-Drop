b6e3f581539586f7ad935cf54228c205d8b00526
KalashT <kalashthakare898@gmail.com>
2025-05-14 15:40:51 +0530

    socketEvent store created

--- frontend/src/store/chatStore.js
+++ frontend/src/store/chatStore.js
@@ -2,126 +2,251 @@ import { axiosInstance } from "@/lib/axios";
 import { toast } from "sonner";
 import { create } from "zustand";
 
-export const chatFunc = create((set,get) =>({
+export const chatFunc = create((set, get) => ({
 
-    selectedGroup:null,
-    messages:[],
-    
+    selectedGroup: null,
+    messages: [],
 
-    sendMessage:async({groupId,text})=>{
-        const {messages} = get();
+
+    sendMessage: async ({ groupId, text }) => {
+        const { messages } = get();
         try {
 
-            console.log(groupId,text)
+            console.log(groupId, text)
 
-            const res = await axiosInstance.post(`messages/send`,{
+            const res = await axiosInstance.post(`messages/send`, {
                 groupId,
                 text
             });
 
             console.log(res.data);
-            
-            set({messages:[...messages,res.data]})
+
+            set({ messages: [...messages, res.data] })
 
 
         } catch (error) {
             toast.error("Error sending message");
-            console.log("Error in sendMessage",error);
+            console.log("Error in sendMessage", error);
         }
     },
 
     getMessages: async (groupId) => {
-    console.log(groupId)
-    try {
-        const res = await axiosInstance.post(`/messages/getMessages`,{groupId});
-        console.log("Fetched from server:", res.data);
-        set({ messages: res.data });
-        return res.data;
-    } catch (error) {
-        toast.error("Failed loading messages");
-        console.error("Error in getMessages:", error);
-        return []; 
+        console.log(groupId)
+        try {
+            const res = await axiosInstance.post(`/messages/getMessages`, { groupId });
+            console.log("Fetched from server:", res.data);
+            set({ messages: res.data });
+            return res.data;
+        } catch (error) {
+            toast.error("Failed loading messages");
+            console.error("Error in getMessages:", error);
+            return [];
+        }
+    },
+
+    addMessage: (newMessage) => {
+        const { messages } = get();
+        set({ messages: [...messages, newMessage] });
+    },
+
+    clearSelectedGroup: () => {
+        set({ selectedGroup: null, messages: [] });
     }
-}
 }))
 
-export const groupFunc = create((set,get)=>({
+export const groupFunc = create((set, get) => ({
 
-    createdGroups:[],
-    memberGroups:[],
+    createdGroups: [],
+    memberGroups: [],
 
-    getGroups:async(data)=>{
+    getGroups: async (data) => {
 
         try {
 
             const res = await axiosInstance.get("/messages/rooms");
             console.log(res.data);
 
-            set({createdGroups:res.data.createdGroups})
-            set({memberGroups:res.data.memberGroups})
-            
+            set({ createdGroups: res.data.createdGroups })
+            set({ memberGroups: res.data.memberGroups })
+
         } catch (error) {
 
-            console.error("Error in chatStore",error)
-            set({createdGroups:[]});
-            set({memberGroups:[]});
-            
+            console.error("Error in chatStore", error)
+            set({ createdGroups: [] });
+            set({ memberGroups: [] });
+
         }
 
     },
 
-    createGroup:async(data)=>{
+    createGroup: async (data) => {
 
         try {
 
             console.log(data)
 
-            const res = await axiosInstance.post("/group/create",data);
+            const res = await axiosInstance.post("/group/create", data);
             toast.success("New group created");
-            
+
+            get().getGroups();
+
+            return res.data;
+
         } catch (error) {
-  
+
             console.error("error in create group function")
             toast.error("Failed to create");
+            return null;
 
         }
 
     },
 
-    deleteGroup:async(groupId)=>{
+    deleteGroup: async (groupId) => {
         try {
 
-            const res = await axiosInstance.post("/group/terminate",{
+            const res = await axiosInstance.post("/group/terminate", {
                 groupId: groupId
-              });
+            });
             toast.success("Group deleted");
+
+            get().getGroups();
+
+            return res.data;
 
         } catch (error) {
 
-            console.error("error in deleting group",error);
+            console.error("error in deleting group", error);
             toast.error("Error deleting group");
-            
+            return null;
+
         }
     },
 
-    addMember:async({groupId,memberEmail})=>{
+    addMember: async ({ groupId, memberEmail }) => {
 
         try {
 
             // console.log(groupId,memberEmail);
 
-            const res = await axiosInstance.post("/group/add-member",{
+            const res = await axiosInstance.post("/group/add-member", {
                 groupId,
-                emails:memberEmail
+                emails: memberEmail
             });
             toast.success("Added member");
-            
+
+            get().getGroups();
+
+            return res.data;
+
         } catch (error) {
 
-            console.error("Error in adding member",error);
+            console.error("Error in adding member", error);
             toast.error("Error adding Member");
-            
+            return null;
+
         }
+    },
+
+    addGroup: (groupData) => {
+
+        const { createdGroups } = get();
+
+        set({ createdGroups: [...createdGroups, groupData] });
+
+    },
+
+    updateGroup: (updatedGroup) => {
+
+        const { createdGroups, memberGroups } = get();
+
+        const updatedCreatedGroups = createdGroups.map(group =>
+            group._id === updatedGroup._id ? updatedGroup : group
+        );
+
+        const updatedMemberGroups = createdGroups.map(group =>
+            group._id === updatedGroup._id ? updatedGroup : group
+        );
+
+        set({
+            createdGroups: updatedCreatedGroups,
+            memberGroups: updatedMemberGroups
+        });
+    },
+
+    removeGroup: (groupId) => {
+
+        const { createdGroups, memberGroups } = get();
+
+        set({
+            createdGroups: createdGroups.filter(group => group._id !== groupId),
+            memberGroups: memberGroups.filter(group => group._id !== groupId)
+        })
+    },
+
+    addUserToGroup: (groupId, user) => {
+
+        const { createdGroups, memberGroups } = get();
+
+        const updatedCreatedGroups = createdGroups.map(group => {
+            if (group._id === groupId) {
+                return {
+                    ...group,
+                    members: [...group.members, user]
+                };
+            }
+
+            return group;
+
+        });
+
+        const updatedMemberGroups = memberGroups.map(group => {
+            if (group._id === groupId) {
+                return {
+                    ...group,
+                    members: [...Grid2X2Plus.members, user]
+                };
+            }
+
+            return group;
+        });
+
+        set({
+            createdGroups: updatedCreatedGroups,
+            memberGroups: updatedMemberGroups
+        });
+    },
+
+    removeUserFromGroup: (groupId, userId) => {
+
+        const { createdGroups, memberGroups } = get();
+
+        const updatedCreatedGroups = createdGroups.map(group => {
+            if (group._id === groupId) {
+                return {
+                    ...group,
+                    members: group.members.filter(member => member._id !== userId)
+                };
+            }
+
+            return group;
+        });
+
+        const updatedMemberGroups = memberGroups.map(group => {
+            if (group._id === groupId) {
+                return {
+                    ...group,
+                    members: group.members.filter(member => member._id !== userId)
+                };
+            }
+
+            return group;
+        });
+
+        set({
+            createdGroups: updatedCreatedGroups,
+            memberGroups: updatedMemberGroups
+        });
     }
 
 }))
---
+++ frontend/src/store/socketEvents.js
@@ -0,0 +1,160 @@
+import { create } from "zustand";
+import { useAuthStore } from "./useAuthStore";
+import { chatFunc, groupFunc } from "./chatStore";
+
+export const useSocketEventStore = create((set, get) => ({
+
+    activeGroupId: null,
+    activeUserId: null,
+
+    setActiveChat: (type, id) => {
+        if (type === 'group') {
+            set({
+                activeGroupId: id, activeUserId: null
+            });
+        } else if (type === 'user') {
+            set({
+                activeUserId: id, activeGroupId: null
+            });
+        }
+    },
+
+    clearActiveChat: () => {
+        set({ activeGroupId: null, activeUserId: null })
+    },
+
+    subscribeToEvents: () => {
+
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        get().unsubscribeFromEvents();
+        get().subscribeToMessages();
+        get().subscribeToGroupEvents();
+        get().subscribeToUserEvents();
+    },
+
+    unsubscribeFromEvents: () => {
+
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        get().unsubscribeFromMessages();
+        get().unsubscribeFromGroupEvents();
+        get().unsubscribeFromUserEvents();
+
+    },
+
+    subscribeToMessages: () => {
+
+        const { activeGroupId } = get();
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.on('newMessage', (newMessage) => {
+            if (activeGroupId && newMessage.groupId === activeGroupId) {
+                chatFunc.getState().addMessage(newMessage);
+            }
+        })
+    },
+
+    unsubscribeFromMessages: () => {
+
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.off("newMessage");
+
+    },
+
+    subscribeToGroupEvents: () => {
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.on("newGroupCreated", (groupData) => {
+            groupFunc.getState().addGroup(groupData);
+        });
+
+        socket.on("groupUpdated", (updatedGroup) => {
+            groupFunc.getState().updateGroup(updatedGroup);
+        });
+
+        socket.on("groupDeleted", (groupId) => {
+            groupFunc.getState().removeGroup(groupId);
+
+            if (get().activeGroupId === groupId) {
+                get().clearActiveChat();
+                chatFunc.getState().clearSelectedGroup();
+            }
+        });
+    },
+
+    unsubscribeFromGroupEvents: () => {
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.off("newGroupCreated");
+        socket.off("groupUpdated");
+        socket.off("groupDeleted");
+    },
+
+    subscribeToUserEvents: () => {
+
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.on('userAddedToGroup', ({ groupId, user }) => {
+            groupFunc.getState().addUserToGroup(groupId, user);
+        });
+
+        socket.on("userRemovedFromGroup", ({ groupId, userId }) => {
+            groupFunc.getState().removeUserFromGroup(groupId, userId);
+
+            if (userId === useAuthStore.getState().user?._id) {
+                if (get().activeGroupId === groupId) {
+                    get().clearActiveChat();
+                    chatFunc.getState().clearSelectedGroup();
+                }
+            }
+        });
+
+        //user Role change code to written here !!!
+
+    },
+
+    unsubscribeFromUserEvents: () => {
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.off("userAddedToGroup");
+        socket.off("userRemovedFromGroup");
+
+    },
+
+    initSocketEvents: () => {
+
+        const socket = useAuthStore.getState().socket;
+        if (!socket) return;
+
+        socket.on("connect", () => {
+            console.log("Socket connected");
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Socket disconnected");
+        });
+
+        get().subscribeToEvents();
+    },
+
+    cleanup: () => {
+        get().unsubscribeFromEvents();
+
+        const socket = useAuthStore.getState().socket;
+        if (socket) {
+            socket.off("connect");
+            socket.off("disconnect");
+        }
+    }
+
+}))
