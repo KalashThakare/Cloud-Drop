95667a421ce6eb57783aa7e498e0f18c7f472264
KalashT <kalashthakare898@gmail.com>
2025-05-19 17:36:31 +0530

    limit set

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,8 +19,8 @@ app.use(cors({
     origin:"http://localhost:3000",
     credentials:true
 }));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({limit:"50mb",extended:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
