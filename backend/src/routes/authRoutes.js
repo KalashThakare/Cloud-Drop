5c00c3f2f7aa13bd4c72e18656d90f0f99c10452
KalashT <kalashthakare898@gmail.com>
2025-05-21 20:21:10 +0530

    bug fix

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { signup,login,logout, checkAuth } from "../controller/auth.Controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import passport from "passport";
 
 const router  = express.Router();
 
@@ -12,29 +11,5 @@ router.post("/login",login);
 router.post("/logout",logout);
 
 router.get("/check",protectRoute,checkAuth);
-
-// OAuth2.0
-
-router.get("/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "http://localhost:3000/Auth",
-    session: true,
-    scope: ["profile", "email"]
-  }),
-  (req, res) => {
-    res.redirect("http://localhost:3000/Main?useDefault=true");
-  }
-);
-
-router.get("/logout", (req, res) => {
-  req.logout(() => {
-    res.clearCookie("connect.sid");
-    res.redirect("http://localhost:3000");
-  });
-});
-
-router.get("/current-user", (req, res) => {
-  res.json(req.user || null);
-});
 
 export default router;
