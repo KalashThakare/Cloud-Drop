1d6607d26a62a0ad4bc3a181a2b0650f12e4c388
KalashT <kalashthakare898@gmail.com>
2025-05-23 22:05:50 +0530

    webhook route

--- backend/src/routes/subscriptionRoutes.js
+++ backend/src/routes/subscriptionRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { subscribeToPlan } from "../controller/Subscribtion/planController.js";
+import { razorpayWebhook } from "../controller/Subscribtion/webhook.js";
 
 
 const router = express.Router();
 
 router.post("/subscribe",subscribeToPlan);
+
+router.post('/webhook/razorpay', express.raw({ type: 'application/json' }), razorpayWebhook);
 
 export default router;
